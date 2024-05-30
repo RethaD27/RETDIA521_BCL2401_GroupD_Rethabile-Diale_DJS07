adbed5f242d5a1fd3014a081732d61eadd9e649f
Rethabile Diale <rethadiale27@gmail.com>
2024-05-30 16:46:40 +0200

    Added the <link> tag for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Header from "./components/Header";
-import Meme from "./components/Meme";
+//import React from "react";
+import Header from "./Header";
+import Meme from "./Meme";
 /**
  * Challenge:
  * - Create a Meme component.
