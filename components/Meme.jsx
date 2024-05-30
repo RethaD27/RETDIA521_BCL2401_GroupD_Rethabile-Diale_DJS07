be76bf07c52c1663ab3d4c6a3c65ae5d4efbd216
Rethabile Diale <rethadiale27@gmail.com>
2024-05-30 13:52:54 +0200

    Getting a random image from the `memesData` array when the "new meme image" button is clicked.

--- components/Meme.jsx
+++ components/Meme.jsx
@@ -1,30 +1,26 @@
 import React from "react";
+import memesData from "../memesData.js";
 
 export default function Meme() {
+  let url;
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+
   return (
     <main>
-      <form className="form">
-        <div>
-          <label htmlFor="top-text">Top Text</label>
-          <input
-            id="top-text"
-            type="text"
-            placeholder="Shut up"
-            className="form--input"
-          />
-        </div>
-        <div>
-          <label>
-            Bottom Text
-            <input
-              type="text"
-              placeholder="and take my money"
-              className="form--input"
-            />
-          </label>
-        </div>
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+      <p>{url}</p>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
     </main>
   );
 }
